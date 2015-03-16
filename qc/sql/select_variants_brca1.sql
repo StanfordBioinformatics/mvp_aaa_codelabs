21ba9de1a963a21aa2b57c5e5c4c865d07086bd6
Greg <gregmcinnes@users.noreply.github.com>
2015-03-16 16:05:54 -0700

    Added alternate bases.

--- qc/sql/select_variants_brca1.sql
+++ qc/sql/select_variants_brca1.sql
@@ -1,9 +1,11 @@
- SELECT
+SELECT
   vars.call_set_name AS sample_name,
   ref.chromosome AS chromosome,
   ref.start AS start,
   vars.var_end AS end,
   ref.reference_bases AS reference_bases,
+  vars.alternate_bases AS alternate_bases,
+  vars.is_variant_call AS is_variant_call,
 FROM (
   SELECT  
     CONCAT('chr', Chr) AS chromosome,
@@ -24,6 +26,8 @@ SELECT
         end AS var_end,
         INTEGER(FLOOR(start / 5000)) AS bin,
         reference_bases,
+        alternate_bases,
+        is_variant_call,
       FROM ( FLATTEN((
         SELECT
           call.call_set_name,
@@ -33,6 +37,7 @@ SELECT
           reference_bases,
           LENGTH(reference_bases) AS ref_len,
           MIN(LENGTH(alternate_bases)) WITHIN RECORD AS alt_len,
+          alternate_bases,
           IF(alternate_bases IS NULL,
             FALSE,
             TRUE) AS is_variant_call,
@@ -52,10 +57,12 @@ SELECT
         var_start,
         var_end,
         bin,
-        reference_bases ) AS vars
+        reference_bases,
+        alternate_bases,
+        is_variant_call ) AS vars
 ON 
   ref.chromosome = vars.chromosome
   AND ref.start = vars.var_start
 ORDER BY 
   start,
-  end       
+  end  
