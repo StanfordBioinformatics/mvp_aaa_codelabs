a37636db120ec1c2c2586a79ce0f85f6ae2e55bd
Greg <gregmcinnes@users.noreply.github.com>
2015-03-18 00:00:04 -0700

    Add sample names to result

--- qc/sql/titv_by_depth.sql
+++ qc/sql/titv_by_depth.sql
@@ -4,20 +4,23 @@ SELECT
     SUM(mutation IN ('A->C', 'C->A', 'G->T', 'T->G',
                      'A->T', 'T->A', 'C->G', 'G->C')) AS transversions,
   #alternate_allele_count,
-  call.DP
+  call.DP,
+  call.call_set_name,
   FROM(
   
   SELECT
       CONCAT(reference_bases, CONCAT(STRING('->'), alternate_bases)) AS mutation,
       COUNT(alternate_bases) WITHIN RECORD AS num_alts,
       #SUM(call.genotype = 1) WITHIN RECORD AS alternate_allele_count,
-      call.DP
+      call.DP,
+      call.call_set_name,
     FROM(
       SELECT
         reference_bases,
         alternate_bases,
         call.genotype,
-        call.DP
+        call.DP,
+        call.call_set_name,
       FROM(FLATTEN((
       [va_aaa_pilot_data.5_genome_test_vcfs_no_calls]), call.DP))
     # Optionally add clause here to limit the query to a particular
@@ -31,5 +34,7 @@ SELECT
     num_alts = 1
       AND reference_bases IN ('A','C','G','T')
       AND alternate_bases IN ('A','C','G','T'))
-    GROUP BY call.DP
-    ORDER BY call.DP
+    GROUP BY 
+      call.DP,
+      call.call_set_name,
+    ORDER BY call.DP DESC
