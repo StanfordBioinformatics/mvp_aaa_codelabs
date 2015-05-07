8b96ab906a7e77be6fd9b0eb3bba7a6e7ce7a6bb
Greg <gregorymcinnes@gmail.com>
2015-05-07 00:42:24 -0700

    Forgot config

---
+++ qc/qc_pipeline/config.py
@@ -0,0 +1,13 @@
+class Config(object):
+    QUERY_REPO = '/Users/gmcinnes/GitHub/mvp_aaa_codelabs/qc/sql'
+    VARIANT_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.5_genome_test_gvcfs_2'
+    EXPANDED_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.5_genome_test_vcfs_2'
+    GENOTYPING_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.5_genome_test_genotyping_vcfs'
+    PATIENT_INFO = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.patient_info'
+    CLIENT_SECRETS = '/Users/gmcinnes/bin/google_genomics/client_secrets.json'
+    PROJECT_NUMBER = 963911152157
+
+    CUTOFFS = {
+        "genotyping_concordance": 0.98,
+
+    }
