cbb7ad304e12f02da2ed5ec38e2ce4af61b2af42
Greg <gregmcinnes@users.noreply.github.com>
2015-09-17 18:00:46 -0700

    Update heterozygous-call-counts-metrics.sql

--- sql/heterozygous-call-counts-metrics.sql
+++ sql/heterozygous-call-counts-metrics.sql
@@ -1,3 +1,7 @@
+# Compute the average and standard deviation of heterzygous call counts for 
+# all genomes.  This query requires that _MAIN_QUERY_ be substituted with
+# the contents of heterozygous-call-counts.sql
+
 SELECT
 ROUND(AVG(O_HET), 3) AS average,
 ROUND(STDDEV(O_HET), 3) AS stddev
