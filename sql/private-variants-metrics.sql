ee097eb578d4ef1e3274521a9cce2d208ea69479
Greg <gregmcinnes@users.noreply.github.com>
2015-09-20 12:45:08 -0700

    Update private-variants-metrics.sql

--- sql/private-variants-metrics.sql
+++ sql/private-variants-metrics.sql
@@ -1,4 +1,5 @@
-# Compute private variants counts for each sample.
+# Compute the average and standard deviation of private variants within the cohort.
+# This query requires that _MAIN_QUERY_ be substituted with the contents of private-snv-counts.sql.
 SELECT 
 ROUND(AVG(private_variant_count), 3) AS average,
 ROUND(STDDEV(private_variant_count), 3) AS stddev
