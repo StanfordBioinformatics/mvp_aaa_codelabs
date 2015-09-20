ce1dc9dd3e383166c55a36c8e63462acd748765e
Greg <gregmcinnes@users.noreply.github.com>
2015-09-20 12:44:26 -0700

    Update private-variants-fail.sql

--- sql/private-variants-fail.sql
+++ sql/private-variants-fail.sql
@@ -1,3 +1,6 @@
+# Identify samples whose private variant count is outside of a defined range.
+# This query requires that _MAIN_QUERY_ be substituted with the contents of private-snv-counts.sql.
+
 SELECT 
 call.call_set_name AS sample_id,
 private_variant_count
